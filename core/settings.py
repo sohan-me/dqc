bddec2eda254612663e2d63facd3fcad78877535
Sohanoor Rahman <78741362+sohan-me@users.noreply.github.com>
2025-01-23 16:43:58 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -206,5 +206,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sohun.me@gmail.com' 
-EMAIL_HOST_PASSWORD = 'jext iiho hxlj vclt' 
+EMAIL_HOST_USER = 'your email' 
+EMAIL_HOST_PASSWORD = 'email_app_password' 
